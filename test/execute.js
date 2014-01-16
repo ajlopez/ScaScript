5ab3ebfa7af5cc27abadd7f4d4d121c5e547cdaa
ajlopez <webmaster@ajlopez.com>
2014-01-16 20:28:46 -0300

    Execute var statement, first test

---
+++ test/execute.js
@@ -0,0 +1,16 @@
+
+var p = require('../lib/parser'),
+    c = require('../lib/context');
+
+function execute(text, ctx) {
+    var parser = p(text);
+    var statement = parser.parse('Statement');
+    return statement.value.execute(ctx);
+}
+
+exports['execute var statement'] = function (test) {
+    var ctx = c();
+    execute('var a = 1;', ctx);
+    test.strictEqual(ctx.getLocalValue('a'), 1);
+};
+
